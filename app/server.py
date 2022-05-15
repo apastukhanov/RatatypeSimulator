df315e4f8bd69ab65021cddcf2c66cfecabe6cf1
aleksandr <apastukhanov@mail.ru>
2022-05-16 00:46:15 +0300

    refactor server.py

--- app/server.py
+++ app/server.py
@@ -40,9 +40,12 @@ def statistics():
     user = User()
     df = Calculator.read_data()
     df = df.loc[df["user"] == user.get_user()]
-    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S.%f').dt.strftime('%d %B %Y, %H:%M')
-    df1 = df.sort_values(by='timestamp', ascending=False).reset_index().drop('index', axis=1).head(7)
-    df2 = df.groupby(by="language")[['speed', 'accuracy']].mean()
+    
+    df0 = df.copy()
+    df0['timestamp'] = pd.to_datetime(df0['timestamp'], format='%Y-%m-%d %H:%M:%S.%f').dt.strftime('%d %B %Y, %H:%M')
+    
+    df1 = df0.sort_values(by='timestamp', ascending=False).reset_index().drop('index', axis=1).head(7)
+    df2 = df0.groupby(by="language")[['speed', 'accuracy']].mean()
 
     fig1 = go.Figure(go.Indicator(
         domain={'x': [0, 1], 'y': [0, 1]},
@@ -96,42 +99,41 @@ def statistics():
                    {'range': [90, 100], 'color': "gray"}],
                'threshold': {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 95}}))
 
-    df = Calculator.read_data()
     df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
     df['timestamp'] = df['timestamp'].dt.strftime('%d.%m.%Y')
 
-    m = df.loc[(df['language'] == 'en') & (df['user'] == user.get_user())].groupby(by=['timestamp'])[
+    en = df.loc[(df['language'] == 'en') & (df['user'] == user.get_user())].groupby(by=['timestamp'])[
         ['speed', 'accuracy']].mean()
 
-    d = df.loc[(df['language'] == 'ru') & (df['user'] == user.get_user())].groupby(by=['timestamp'])[
+    ru = df.loc[(df['language'] == 'ru') & (df['user'] == user.get_user())].groupby(by=['timestamp'])[
         ['speed', 'accuracy']].mean()
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
     fig.add_trace(go.Scatter(
-        x=m.index,
-        y=m['accuracy'].values,
+        x=en.index,
+        y=en['accuracy'].values,
         mode="lines",
         name="accuracy EN"
     ), secondary_y=True)
 
     fig.add_trace(go.Scatter(
-        x=m.index,
-        y=m['speed'].values,
+        x=en.index,
+        y=en['speed'].values,
         mode="lines",
         name="speed EN",
     ))
 
     fig.add_trace(go.Scatter(
-        x=d.index,
-        y=d['speed'].values,
+        x=ru.index,
+        y=ru['speed'].values,
         mode="lines",
         name="speed RU",
     ))
 
     fig.add_trace(go.Scatter(
-        x=d.index,
-        y=d['accuracy'].values,
+        x=ru.index,
+        y=ru['accuracy'].values,
         mode="lines",
         name="accuracy RU",
     ), secondary_y=True)
@@ -144,9 +146,6 @@ def statistics():
 
     return render_template('statistics.html',
                            tables1=[df1.to_html(classes='data', header="true", index=False)],
-                           titles1=df1.columns.values,
-                           tables2=[df2.to_html(classes='data', header="true")],
-                           titles2=df2.columns.values,
                            graphJSON1=graphJSON1,
                            graphJSON2=graphJSON2,
                            graphJSON3=graphJSON3,
